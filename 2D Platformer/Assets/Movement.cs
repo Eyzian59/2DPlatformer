427ed6b5e225ff80b510a80ef1805a6afd4d12b7
MateoVerde <matthewjgreen4@gmail.com>
2024-06-24 15:49:57 -0500

    animation

--- 2D Platformer/Assets/Movement.cs
+++ 2D Platformer/Assets/Movement.cs
@@ -5,29 +5,36 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Animator animator;
     public float speed;
-    public float jump;
+    public float jumpForce;
     private float xMovement;
-
     public bool grounded;
 
     // Update is called once per frame
     void Update()
     {
-        xMovement = Input.GetAxis("Horizontal");
-
-        rb.velocity = new Vector2(xMovement * speed, rb.velocity.y);
-
         if(Input.GetButtonDown("Jump") && grounded == true)
         {
-            rb.AddForce(new Vector2(rb.velocity.x, jump));
+            rb.velocity = Vector2.up * jumpForce;
+            animator.SetBool("isJumping", true);
         }
+    }
+
+    void FixedUpdate()
+    {
+        xMovement = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("Speed", Mathf.Abs(xMovement));
+
+        rb.velocity = new Vector2(xMovement * speed, rb.velocity.y);
     }
 
     private void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.CompareTag("Ground"))
         {
+            animator.SetBool("isJumping", false);
             grounded = true;
         }
     }
